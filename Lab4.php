fd51aee68d47f232fcb8d3fffdfd2259d298d5c5
Руслан Шаяхметов <58970010+lenor421@users.noreply.github.com>
2020-11-24 00:06:51 +0500

    Добавил таблицу вклады

--- Lab4.php
+++ Lab4.php
@@ -47,14 +47,36 @@ while ($row=mysqli_fetch_array($result)){
  echo "<td>" . $row['Name'] . "</td>";
  echo "<td>" . $row['Proz'] . "</td>";
  echo "<td>" . $row['Naz_banka'] . "</td>";
- echo "<td><a href='lab4-3.php?id=" . $row['id']
+ echo "<td><a href='lab5-3.php?id=" . $row['id']
 . "'>Редактировать</a></td>"; 
- echo "<td><a href='lab4-5.php?id=" . $row['id']
+ echo "<td><a href='lab5-5.php?id=" . $row['id']
 . "'>Удалить</a></td>"; 
 echo "</tr>";}print "</table>";
 $num_rows1 = mysqli_num_rows($result); 
 print("<P>Всего программ: $num_rows1 </p>");
 ?>
 <p> <a href="lab5-1.php"> Добавить программу </a>
+<h2>Вклады:</h2>
+<table border="1">
+<tr> 
+ <th> Дата создания вклада </th> 
+ <th> Наименование программы депозита </th> 
+ <th> Стартовая сумма вклада </th> </tr>
+<?php
+$result=mysqli_query ($connect, "SELECT id, date,  id_prog, Naz_prog, start_sum FROM deposit");
+while ($row=mysqli_fetch_array($result)){
+ echo "<tr>";
+ echo "<td>" . $row['date'] . "</td>";
+ echo "<td>" . $row['Naz_prog'] . "</td>";
+ echo "<td>" . $row['start_sum'] . "</td>";
+ echo "<td><a href='lab50-3.php?id=" . $row['id']
+. "'>Редактировать</a></td>"; 
+ echo "<td><a href='lab50-5.php?id=" . $row['id']
+. "'>Удалить</a></td>"; 
+echo "</tr>";}print "</table>";
+$num_rows = mysqli_num_rows($result); 
+print("<P>Всего программ: $num_rows </p>");
+?>
+<p> <a href="lab50-1.php"> Добавить вклад </a>
 	</body>
 </html>
