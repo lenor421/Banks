6c0b2f600afbf945559c8e3f81e24fa42e22e2c2
Руслан Шаяхметов <58970010+lenor421@users.noreply.github.com>
2020-11-24 01:06:27 +0500

    Добавил удаление таблицы вклады

---
+++ lab50-5.php
@@ -0,0 +1,8 @@
+<?php
+$connect = mysqli_connect("localhost", "root", "root", "Banks");
+mysqli_query($connect, "SET NAMES utf8");
+ $zapros="DELETE FROM deposit WHERE id=" . $_GET['id'];
+ mysqli_query($connect, $zapros);
+ header("Location: Lab4.php");
+ exit;
+?>
